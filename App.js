31f84b581c8565b5bff1b7bae1c21fcde296edac
IvanovV <ivanovv@incom-sr.ru>
2021-01-29 14:17:21 +0300

    fixed

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// https://www.youtube.com/watch?v=7KwtXNoYQEY&list=WL&index=6&t=2052s&ab_channel=%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BB%D0%B5%D0%BD%D0%9C%D0%B8%D0%BD%D0%B8%D0%BD
 // react-native
 // mobile ToDo
 
